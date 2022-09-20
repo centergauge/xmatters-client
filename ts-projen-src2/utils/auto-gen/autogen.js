bf4fff6c18aa2ff2ec27a16af59d430822657755
Brian Abbott <briancabbott@gmail.com>
2022-09-20 09:46:26 -0700

    updated for auto-gen

--- ts-projen-src2/utils/auto-gen/autogen.js
+++ ts-projen-src2/utils/auto-gen/autogen.js
@@ -1,25 +1,19 @@
-
 // /// Get each model object entry
 // document.querySelectorAll('ul.tocify-header').forEach(a => console.log(a.children[0].attributes['data-unique']))
-
-
 // // Get each sub-component/supporting function of each model object.
 // document.querySelectorAll('ul.tocify-header').forEach(a => Array.from(a.children[1].children).forEach(el => console.log(el.attributes['data-unique'])))
-
-
 import puppeteer from "puppeteer";
-
-export async function FetchLatestXMattersAPIModel(): Promise<Map<string, Array<{name: string, subComponents: Array<string>}>>> {
-    const modelData: Array<{name: string, subComponents: Array<string>}> = await puppeteer.launch().then(async browser => {
+export async function FetchLatestXMattersAPIModel() {
+    const modelData = await puppeteer.launch().then(async (browser) => {
         const page = await browser.newPage();
         await page.goto('https://help.xmatters.com/xmapi/index.html');
         await page.waitForSelector('ul.tocify-header');
         const modelObjects = await page.evaluate(() => {
-            const modObjsArr: Array<{name: string, subComponents: Array<string>}> = [];
+            const modObjsArr = [];
             document.querySelectorAll('ul.tocify-header').forEach(a => {
-                const modObj:{name: string, subComponents: Array<string>} = {
+                const modObj = {
                     name: a.children[0].attributes['data-unique'].value,
-                    subComponents: new Array<string>()
+                    subComponents: new Array()
                 };
                 Array.from(a.children[1].children).forEach(el => {
                     modObj.subComponents.push(el.attributes['data-unique'].value);
@@ -31,49 +25,49 @@ export async function FetchLatestXMattersAPIModel(): Promise<Map<string, Array<{
         await browser.close();
         return modelObjects;
     });
-
     // Now group the returned data for generation
-    const groupedRoots = new Map<string, Array<{name: string, subComponents: Array<string>}>>();
+    const groupedRoots = new Map();
     for (let mel of modelData) {
         let unhyphenatedRoot = "";
         if (mel.name.includes('-')) {
             unhyphenatedRoot = mel.name.split('-')[0];
         }
-
         let singularRoot = "";
         if (unhyphenatedRoot === "" && mel.name.endsWith('s')) {
             singularRoot = mel.name.slice(0, -1);
-        } else if (unhyphenatedRoot.length > 0) {
+        }
+        else if (unhyphenatedRoot.length > 0) {
             if (unhyphenatedRoot.length > 0 && unhyphenatedRoot.endsWith('s')) {
                 singularRoot = unhyphenatedRoot.slice(0, -1);
-            } else {
+            }
+            else {
                 singularRoot = unhyphenatedRoot;
             }
-        } else {
+        }
+        else {
             singularRoot = mel.name;
         }
-
         if (singularRoot.length === 0) {
             // problem....
             console.log("A PROBLEM was encountered with model-gen - there is no available component/object group for the retreived item: " + mel.name);
-            console.log("The full object was:")
+            console.log("The full object was:");
             console.log(JSON.stringify(mel, null, 4));
-            process.exit(1)
-        } else {
+            process.exit(1);
+        }
+        else {
             if (singularRoot in groupedRoots) {
                 groupedRoots[singularRoot].push(mel);
-            } else {
-                const arr = new Array<{name: string, subComponents: Array<string>}>();
+            }
+            else {
+                const arr = new Array();
                 arr.push(mel);
                 groupedRoots[singularRoot] = arr;
             }
         }
     }
-
     // read input from the user
-    console.log(JSON.stringify(groupedRoots, null, 4))
+    console.log(JSON.stringify(groupedRoots, null, 4));
     return groupedRoots;
 }
-
 let apiModel = await FetchLatestXMattersAPIModel();
-console.log(JSON.stringify(apiModel, null, 4))
+console.log(JSON.stringify(apiModel, null, 4));
